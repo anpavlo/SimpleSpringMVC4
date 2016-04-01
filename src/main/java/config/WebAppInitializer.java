e40650612871fa4bf91fb0c48bb6ce1fc0f51537
Roman Golyuk <roman.golyuk@gmail.com>
2016-04-01 21:49:21 +0300

    Added Spring Security

--- src/main/java/config/WebAppInitializer.java
+++ src/main/java/config/WebAppInitializer.java
@@ -1,34 +1,27 @@
 package config;
 
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRegistration;
- 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
- 
-public class WebAppInitializer implements WebApplicationInitializer {
- 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer  {
+
     @Override
-    public void onStartup(ServletContext container) {
-        // Create the 'root' Spring application context
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(ServiceConfig.class);
- 
-        // Manage the lifecycle of the root application context
-        container.addListener(new ContextLoaderListener(rootContext));
- 
-        // Create the dispatcher servlet's Spring application context
-        AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
-        dispatcherServlet.register(MvcConfig.class);
-             
-        // Register and map the dispatcher servlet
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherServlet));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-         
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfig.class, ServiceConfig.class};
     }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        
+        return new String[]{
+                "/"
+        };
+    }
+
  
  }
